03d853233d2e33c7601147a1910a2deb63f5c7a3
Елена Петрова <elenapetrova23012016@gmail.com>
2022-06-19 23:14:35 +0100

    refactor: add buttons in video page pagination, sass placeholder for flex-row, disable page scroll under modal window

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,11 +1,13 @@
-// let close = document.getElementsByClassName('.close-popup')
 let bookingPopup = document.querySelector('.book-ticket-popup')
 let blur = document.querySelector('.blur')
+let body = document.body;
 function closePopup () {
     blur.style.display = 'none'
+    body.style.overflowY = 'auto'
 }
 
 document.querySelector('.close-popup').addEventListener('click', closePopup)
 document.querySelector('.buy-button').addEventListener('click', function (){
     blur.style.display = 'flex'
+    body.style.overflowY = 'hidden'
 })
