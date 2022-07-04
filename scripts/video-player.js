7f02ffa298f20ea033894f8e4020db129fdc5e84
Елена Петрова <elenapetrova23012016@gmail.com>
2022-07-04 23:35:15 +0100

    start: video slider feat: adaptive section welcome, tour, explore, video, gallery, base headers fonts

---
+++ scripts/video-player.js
@@ -0,0 +1,45 @@
+// const video_buttons = document.querySelectorAll(".video-button")
+// const video_slides = document.querySelectorAll('.video-slide')
+//
+//
+// let index = 0;
+//
+// const activeButton = m => {
+//     for (let button of video_buttons) {
+//         button.classList.remove('active')
+//     }
+//     video_buttons[m].classList.add('active')
+// }
+//
+// const  activeVideoSlide = n => {
+//     for( let slide of video_slides) {
+//         slide.classList.remove('active')
+//     }
+//     video_slides[n].classList.add('active')
+// }
+//
+// const nextVideoSlide = () => {
+//     if (index === video_slides.length - 1) {
+//         index = 0;
+//         activeButton(index)
+//         activeVideoSlide(index)
+//     } else {
+//         index++;
+//         activeVideoSlide(index)
+//         activeButton(index)
+//     }
+// }
+//
+// const prevVideoSlide = () => {
+//     if (index === 0) {
+//         index = video_slides.length - 1;
+//         activeVideoSlide(index)
+//         activeButton(index)
+//
+//     } else {
+//         index--;
+//         activeVideoSlide(index)
+//         activeButton(index)
+//     }
+// }
+
